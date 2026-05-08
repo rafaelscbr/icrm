1aa030c8ee43dcf8f0418d86b79c7ee978ecc3f8
Rafael Alves de Souza <rafael.info.souza@gmail.com>
2026-05-08 15:06:17 -0300

    Alterações

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c5fd805d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.5nimb1q8998"
+    "revision": "0.mp2b7kt8p3c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
