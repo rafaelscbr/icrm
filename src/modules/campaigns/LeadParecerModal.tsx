37271b1e1a616ab5b67acd077dfe90bf5926315f
Rafael Alves de Souza <rafael.info.souza@gmail.com>
2026-06-01 23:53:34 -0300

    auto: LeadParecerModal.tsx [skip ci]

--- src/modules/campaigns/LeadParecerModal.tsx
+++ src/modules/campaigns/LeadParecerModal.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
 import { CheckCircle2, GitMerge, Sparkles, ArrowRight } from 'lucide-react'
 import { Modal } from '../../components/ui/Modal'
 import { Button } from '../../components/ui/Button'
